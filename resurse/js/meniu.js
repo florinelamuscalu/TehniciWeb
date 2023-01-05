687067e1e3edf46835c4841e5de86b6c1d39cd7e
florinelamuscalu <mflorinelagarbiela@gmail.com>
2023-01-05 11:02:24 +0200

    submeniu

--- resurse/js/meniu.js
+++ resurse/js/meniu.js
@@ -11,6 +11,18 @@ window.addEventListener("load", function () {
         }
 
         //document.getElementById("header").innerHTML = "Width: " + w + ", " + "Height: " + h
+
+
+        
+        let submeniu_list= document.querySelectorAll("ul li ul li a");
+        let submeniu_list2 =document.querySelectorAll("ul li ul li ul li a")
+        for (let submeniu of submeniu_list){
+            submeniu.style.display="none";
+        }
+
+        for (let submeniu2 of submeniu_list2){
+            submeniu2.style.display="none";
+        }
     }
 
 
